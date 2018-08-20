15a43d3b2150eb5922a81ac066da10a2bd35fe63
LMiyano <42355123+LMiyano@users.noreply.github.com>
2018-08-20 14:05:49 -0300

    Add files via upload

--- src/br/com/db1/ExercicioCollections.java
+++ src/br/com/db1/ExercicioCollections.java
@@ -1,14 +1,40 @@
 package br.com.db1;
 
+import java.text.Collator;
+import java.util.Collections;
 import java.util.List;
 
 public class ExercicioCollections {
-	
-	public List<String> listaDeCores(List<String> cores)
-	{	
-		return cores;		
+
+	public List<String> listaDeCores(List<String> cores) {
+		return cores;
 	}
 
-	
-}
+	public Integer quantidadeLista(List<String> cores) {
+		return cores.size();
+	}
 
+	public List<String> apagarElementos(List<String> pessoas,List<String> apagar) {
+		pessoas.removeAll(apagar);
+		return pessoas;
+
+	}
+
+	public List<String> apagarSegundoElemento(List<String> cidades) {
+		cidades.remove(1);
+		return cidades;
+
+	}
+
+	public List<String> coresOrdenadas(List<String> cores) {
+		Collections.sort(cores, Collator.getInstance());		
+		return cores;
+	}
+	
+	public List<String> apagarCor(List<String> cores) {
+		
+		return cores;
+
+	}
+
+}
